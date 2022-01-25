cb5b7acaefebcbb2c6b398cd5ae87cc31bd9d86a
Serdar <89738183+IamSerdar@users.noreply.github.com>
2022-01-25 19:35:29 +0500

    Update txt_file_printer.py

--- txt_file_printer.py
+++ txt_file_printer.py
@@ -3,28 +3,21 @@ import win32ui
 
 x = 0
 y = 50
+printer_name = "EPSON L382 Series (копия 1)"
+# if your printer is standard, replace the printer_name:
+# win32print.GetDefaultPrinter()
 
 fd = open("file.txt", "r", encoding = "utf-8")
 input_string = fd.read()
 multi_line_string = input_string.splitlines()
 
 hDC = win32ui.CreateDC()
-hDC.CreatePrinterDC("EPSON L382 Series (копия 1)")
+hDC.CreatePrinterDC()
 hDC.StartDoc("Printing...")
 hDC.StartPage()
 for line in multi_line_string:
-    hDC.TextOut(x, y, line)
-    y+=50
-hDC.EndPage()
-hDC.EndDoc()
-
-hDC = win32ui.CreateDC()
-hDC.CreatePrinterDC("BIXOLON SRP-350plus (копия 1)")
-hDC.StartDoc("Printing...")
-hDC.StartPage()
-for line in multi_line_string:
-    hDC.TextOut(x, y, line)
-    y+=50
+hDC.TextOut(x, y, line)
+y+=50
 hDC.EndPage()
 hDC.EndDoc()
 fd.close()
